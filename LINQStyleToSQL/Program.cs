2be5403e0017a89de76613cdd23b1e9b88953282
Darius Wattimena <dariuswattimena@hotmail.com>
2020-07-08 22:45:03 +0200

    Add project files.

---
+++ LINQStyleToSQL/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace LINQStyleToSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var group1 = new Group
+            {
+                Name = "Group 1",
+                Value = 10
+            };
+
+            var group2 = new Group
+            {
+                Name = "Group 2",
+                Value = 20
+            };
+
+            User[] users = { new User
+            {
+                Name = "Test",
+                Age = 20,
+                Groups = new[] {group1, group2}
+            }, new User
+            {
+                Name = "Test 2",
+                Age = 10,
+                Groups = new[] { group2 }
+            }, new User
+            {
+                Name = "Test 3",
+                Age = 50,
+                Groups = new[] { group1 }
+            }};
+
+            var results = users.Select(user => new {user.Name, user.Groups})
+                //.Where(user => user.Name == "Test")
+                .Include(x => x.Groups, (groups => groups.Select(group => group.Name)));
+
+            foreach (var result in results)
+            {
+                foreach (var group in result.Item2)
+                {
+                    Console.WriteLine(new {result.Item1, group});
+                }
+            }
+        }
+
+        class User
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public Group[] Groups { get; set; }
+        }
+
+        class Group
+        {
+            public string Name { get; set; }
+            public int Value { get; set; }
+        }
+    }
+}
---
+++ LINQStyleToSQL/Queries.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+namespace LINQStyleToSQL
+{
+    static class Queries
+    {
+        //TODO helper function should be replaced with SQL
+        private static Y Then<T, Y>(this T item, Expression<Func<T, Y>> filter) => filter.Compile()(item);
+
+        public static IEnumerable<Y> Select<T, Y>(this IEnumerable<T> items, Expression<Func<T, Y>> filter)
+        {
+            Console.WriteLine(filter);
+
+            //TODO do SQL stuff here
+
+            foreach (var item in items)
+            {
+                yield return item.Then(filter);
+            }
+        }
+
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> items, Expression<Func<T, bool>> filter)
+        {
+            Console.WriteLine(filter);
+
+            //TODO do SQL stuff here
+
+            foreach (var item in items)
+            {
+                if (item.Then(filter))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<(T, U)> Include<T, Y, U>(this IEnumerable<T> items, Expression<Func<T, Y>> includeItemsFilter, Expression<Func<Y, U>> filter)
+        {
+            foreach (var item in items)
+            {
+                var includeItem = item.Then(includeItemsFilter);
+                var filteredIncludeItem = includeItem.Then(filter);
+                yield return (item, filteredIncludeItem);
+            }
+        }
+    }
+}
